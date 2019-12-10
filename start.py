6a161dddc6d32d6db2de147abb6f71ae9d397863
chandelurewang@qq.com <chandelurewang@qq.com>
2019-12-10 20:07:48 +0800

    first commit

--- start.py
+++ start.py
@@ -1,86 +1,33 @@
 # coding=utf-8  
-import json
-import re
-from seleniumrequests import Chrome
-from selenium.webdriver.chrome.options import Options
 import sys
 import getopt
-import time
-
-
-class TiebaAutoSign:
-    def __init__(self, cookies_file_path):
-        options = Options()
-        options.add_argument('--no-sandbox')
-        options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        self.driver = Chrome(options=options)
-        self.driver.implicitly_wait(20)
-        self.driver.delete_all_cookies()
-        self.tieba_url = "https://tieba.baidu.com/"
-        self.cookies_file_path = cookies_file_path
-        self.sign_url = 'https://tieba.baidu.com/sign/add'
-
-    def add_cookies(self):
-        self.driver.get(self.tieba_url)
-        with open(self.cookies_file_path, "r") as fp:
-            list_cookies = json.loads(fp.readline())
-            for cookie in list_cookies:
-                if 'expiry' in cookie:
-                    del cookie['expiry']
-                self.driver.add_cookie(cookie)
-
-    def get_forum_name_list(self):
-        self.add_cookies()
-        self.driver.get(self.tieba_url)
-        content = self.driver.page_source
-        pattern = re.compile(r"\"user_id\"[^}]*\"is_sign\":0")
-        forum_info_list = pattern.findall(content)
-        forum_name_list = []
-        for forum_info in forum_info_list:
-            json_item = json.loads('{' + forum_info + '}')
-            forum_name = json_item['forum_name']
-            forum_name_list.append(forum_name)
-        return forum_name_list
-
-    @staticmethod
-    def echo(msg):
-        print('[{}]:{}'.format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), msg))
-
-    def auto_sign(self):
-        success = 0
-        for forum_name in self.get_forum_name_list():
-            sign_data = {
-                'ie': 'utf-8',
-                'kw': forum_name,
-                'tbs': '6868ce0283f2fb151561865643'
-            }
-            sign_res = self.driver.request('POST', self.sign_url, data=sign_data)
-            if eval(sign_res.text)["no"] == 0:
-                self.echo("{}吧签到成功！".format(forum_name))
-                success = success + 1
-        self.echo('共签到成功{}个贴吧。'.format(success))
-        self.driver.close()
-        self.driver.quit()
+from tiebaautosign import TiebaAutoSign
 
 
 def main(argv):
     cookies_file_path = ''
     try:
-        opts, args = getopt.getopt(argv, "hc:")
+        opts, args = getopt.getopt(argv, "-h-t:-c:", ['help', 'test', 'cookies_file='])
     except getopt.GetoptError:
         print('start.py -c <cookies_file_path>')
         sys.exit(2)
     for opt, arg in opts:
-        if opt == '-h':
+        if opt in ('-h', '--help'):
             print('start.py -c <cookies_file_path>')
             sys.exit()
-        elif opt == '-c':
+        if opt in ('-t', '--test'):
+            if cookies_file_path:
+                tieba_auto_sign = TiebaAutoSign(cookies_file_path)
+            else:
+                tieba_auto_sign = TiebaAutoSign()
+            tieba_auto_sign.test()
+            sys.exit()
+        if opt in ('-c', '--cookies_file'):
             cookies_file_path = arg
-        else:
-            print('start.py -c <cookies_file_path>')
-            sys.exit()
-    tieba_auto_sign = TiebaAutoSign(cookies_file_path)
+    if cookies_file_path:
+        tieba_auto_sign = TiebaAutoSign(cookies_file_path)
+    else:
+        tieba_auto_sign = TiebaAutoSign()
     tieba_auto_sign.auto_sign()
 
 
---
+++ tiebaautosign.py
@@ -0,0 +1,110 @@
+from seleniumrequests import Chrome
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+import time
+import json
+import re
+
+
+class TiebaAutoSign(object):
+    """
+    Attributes:
+        cookies_file_path: 存放登陆所需cookies文件的路径
+        driver：selenium.webdriver的一个实例化
+        tieba_url: 百度贴吧主页URL
+        sign_url: 百度贴吧签到URL
+    """
+
+    @staticmethod
+    def init_driver():
+        """初始化driver"""
+        options = Options()
+        options.add_argument('--no-sandbox')
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        driver = Chrome(options=options)
+        driver.implicitly_wait(20)
+        driver.delete_all_cookies()
+        return driver
+
+    @staticmethod
+    def echo(msg):
+        """打印信息函数"""
+        print('[{}] {}'.format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), msg))
+
+    def __init__(self, cookies_file_path="./cookies.txt", tieba_url="https://tieba.baidu.com/",
+                 sign_url='https://tieba.baidu.com/sign/add'):
+        """类初始化"""
+        self.driver = self.init_driver()
+        self.tieba_url = tieba_url
+        self.cookies_file_path = cookies_file_path
+        self.sign_url = sign_url
+
+    def add_cookies(self):
+        """为driver添加cookies"""
+        self.driver.get(self.tieba_url)
+        try:
+            with open(self.cookies_file_path, "r") as fp:
+                list_cookies = json.loads(fp.readline())
+                for cookie in list_cookies:
+                    if 'expiry' in cookie:
+                        del cookie['expiry']
+                    self.driver.add_cookie(cookie)
+        except FileNotFoundError:
+            self.echo("未发现cookies文件，请输入正确的路径")
+
+    def get_forum_name_list(self):
+        """获取用户关注且并未签到的贴吧列表"""
+        forum_name_list = []
+        if self.sign_up():
+            content = self.driver.page_source
+            pattern = re.compile(r"\"user_id\"[^}]*\"is_sign\":0")
+            forum_info_list = pattern.findall(content)
+            for forum_info in forum_info_list:
+                json_item = json.loads('{' + forum_info + '}')
+                forum_name = json_item['forum_name']
+                forum_name_list.append(forum_name)
+            return forum_name_list
+        else:
+            self.echo("登录失败。")
+            self.exit_driver()
+
+    def exit_driver(self):
+        """退出driver"""
+        self.driver.close()
+        self.driver.quit()
+
+    def sign_up(self):
+        """判断是否成功登录"""
+        self.add_cookies()
+        self.driver.get(self.tieba_url)
+        try:
+            self.driver.find_element_by_id('my_tieba_mod')
+            return True
+        except NoSuchElementException:
+            return False
+
+    def auto_sign(self):
+        """实现一键签到"""
+        success = 0
+        for forum_name in self.get_forum_name_list():
+            sign_data = {
+                'ie': 'utf-8',
+                'kw': forum_name,
+                'tbs': '6868ce0283f2fb151561865643'
+            }
+            sign_res = self.driver.request('POST', self.sign_url, data=sign_data)
+            if eval(sign_res.text)["no"] == 0:
+                self.echo("{}吧签到成功！".format(forum_name))
+                success = success + 1
+        self.echo('共签到成功{}个贴吧。'.format(success))
+        self.exit_driver()
+
+    def test(self):
+        """测试cookies是否可以使用"""
+        if self.sign_up():
+            self.echo("测试成功，cookies可以正常使用。")
+            self.exit_driver()
+        else:
+            self.echo("测试失败,请更换可用的cookies。")
+            self.exit_driver()
