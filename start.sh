6c75d9570cf33c0270ca2775426a995011620f0f
root <root@mail.chandelure.com>
2019-12-10 22:16:28 +0800

    修复bug

--- start.sh
+++ start.sh
@@ -5,21 +5,21 @@ python="${dir}/venv/bin/python"
 start_file_path="${dir}/start.py"
 cookies_file_path="${dir}/cookies.txt"
 log_path="/var/log/tieba.log"
-cmd="${python} ${start_file_path} -c ${cookies_file_path} >> ${log_path} 2>&1"
+cmd="${python} ${start_file_path} -c ${cookies_file_path} | tee ${log_path}"
 
 is_file_exist(){
-    file_path="$1"
-    if [ -f "$file_path" ]; then
-            return 1
+    local file_path="$1"
+    if [[ -f "$file_path" ]]; then
+        return 0
     else
-            error "文件${file_path}不存在"
+        error "文件${file_path}不存在"
     fi
 }
 
 is_dir_exist(){
-    dir_path="$1"
-    if [ -d "$dir_path" ]; then
-        return 1
+    local dir_path="$1"
+    if [[ -d "$dir_path" ]]; then
+        return 0
     else
         error "目录${dir_path}不存在"
     fi
@@ -35,12 +35,11 @@ msg() {
 
 main(){
     is_dir_exist "${dir}"
-    for file_path in "${python}" "${start_file_path}" "${cookies_file_path}"
+    for file_path in "${start_file_path}" "${python}" "${cookies_file_path}"
     do
-        is_file_exist ${file_path}
+	is_file_exist ${file_path}
     done
-    if [ $? -eq 0 ]; then
-        msg "开始进行一键签到......"
+    if [[ $? -eq 0 ]]; then
         eval ${cmd}
     else
         error "一键签到失败。"
