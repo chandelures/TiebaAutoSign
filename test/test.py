c6b38e65ec20dff1f0d69f58270a776f5bede3e7
chandelures <chandelurewang@gmail.com>
2021-09-18 21:54:34 +0800

    test: add sign test

--- test/test.py
+++ test/test.py
@@ -1,17 +1,27 @@
 #!venv/bin/python
 import unittest
-from src.login import BaiduLogin
+from src.sign import BaiduAutoSign
 
 
 class AutoSignTest(unittest.TestCase):
     def setUp(self):
-        self.baiduLogin = BaiduLogin(verbose=False)
+        self.baiduautosign = BaiduAutoSign(verbose=False)
+        self.baiduautosign.init_loginer()
 
     def test_is_login(self):
-        self.assertEqual(self.baiduLogin.login(), True)
+        self.assertEqual(self.baiduautosign.loginer.is_login, True)
+
+    def test_sign(self):
+        tbs = self.baiduautosign.get_tbs()
+        self.assertNotEqual(tbs, '')
+        forums_iter = self.baiduautosign.get_followed_forums()
+
+        forum = next(forums_iter)
+        if forum:
+            self.assertTrue(self.baiduautosign.sign(forum, tbs))
 
     def tearDown(self):
-        self.baiduLogin.close()
+        self.baiduautosign.close_loginer()
 
 
 if __name__ == "__main__":
